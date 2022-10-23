0aaf75c93f7e7596c01179fadaa357e77b7d9c49
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-22 21:41:22 -0300

    Atualizar página ao cadastrar item.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ const form = document.getElementById("novoItem")
 const lista = document.getElementById('lista')
 const items = JSON.parse(localStorage.getItem('items')) || []
 
+// A iteração a seguir preenche a lista com os dados do LocalStorage.
 items.forEach( (elemento) => {
-    console.log(elemento.nome, elemento.quantidade)
+    criaElemento(elemento)
 })
 
+// A submissão do formulário controla a sequência geral da interação.
 form.addEventListener("submit", (evento) => {
     // Sem o preventDefault, o formulário será enviado  
     // e o comportamento não será percebido. 
@@ -13,33 +15,35 @@ form.addEventListener("submit", (evento) => {
     
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
-    criaElemento(nome.value, quantidade.value)
+
+    // Criação do array que será guardado no LocalStorage.
+    const itemAtual = {
+        "nome" : nome.value,
+        "quantidade" : quantidade.value,
+    }
+
+    criaElemento(itemAtual)
+    
+    items.push(itemAtual)
+
+    localStorage.setItem("items", JSON.stringify(items))
 
     // Limpando o formulário após a inserção.
     nome.value = ""
     quantidade.value = ""
 })
 
-function criaElemento(nome, quantidade) {
+// A função criaElemento trata da exibição dos dados na listagem.
+function criaElemento(itemAtual) {
     const novoItem = document.createElement('li')
     novoItem.classList.add('item')
     
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    numeroItem.innerHTML = itemAtual.quantidade
         
     // Adiciona o elemento novoItem 
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += itemAtual.nome
     
     lista.appendChild(novoItem)
-
-    // Criação do array que será guardado no LocalStorage.
-    const itemAtual = {
-        "nome" : nome,
-        "quantidade" : quantidade,
-    }
-
-    items.push(itemAtual)
-
-    localStorage.setItem("item", JSON.stringify(items))
 }
